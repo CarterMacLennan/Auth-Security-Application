f0f521e4d07741f261d98072f0fd70b83ff27094
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-21 17:41:16 -0400

    Added initial template code & password encryption

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const encrypt = require("mongoose-encryption"); 
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/secretsApp", {useNewUrlParser: true ,useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String,
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", async (req,res) => {
+    let newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    });
+    console.log(req.body.username,req.body.password);
+    newUser.save( err => {
+        if (err){
+            console.log(err);
+        }
+        else {
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+    User.findOne({email: req.body.username}, (err, user) => {
+        if (err) {
+            console.log(err);
+        }
+        else if (user.password === req.body.password){
+            res.render("secrets");
+        }
+        else {
+            console.log("Invalid UserName");
+        }
+    })
+});
+
+app.listen(3000, () => {
+    console.log("Running on port 3000....");
+})
