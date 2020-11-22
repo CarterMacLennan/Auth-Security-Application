e33bea7c4119817afc771a454f5072974ade5a1d
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-22 19:45:39 -0400

    Allowed users to submit their secrets anonymously & view other secrets

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
     googleId: String,
+    secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -92,8 +93,21 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
+    User.find({"secret": {$ne: null}}, (err, users) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (users) {
+                res.render("secrets", {usersWithSecrets: users});
+            }
+        }
+    });
+});
+
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated()){
+        res.render("submit");
     }
     else {
         res.redirect("/login");
@@ -134,6 +148,24 @@ app.post("/login", (req, res) => {
                 res.redirect("/secrets");
             });
         }        
+    });
+});
+
+app.post("/submit", (req, res) => {
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, (err, user) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (user) {
+                user.secret = submittedSecret;
+                user.save(() => {
+                    res.redirect("/secrets");
+                });
+            }
+        }
     });
 });
 
